56580fb12673fee3770b1d31083753b1593f809c
sagan <sagan@syneforge.com>
2020-08-21 18:18:20 +0300

    added model

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+export function row(content, styles = '') {
+    const style = `margin: 0; ${styles}`
+    return `<div class="row" style="${style}">${content}</div>`
+}
+export function col(content) {
+    return `<div class="col-sm">${content}</div>`
+}
